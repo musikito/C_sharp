45464c11f24e02fcc9edf7172e62776a1b5f90cd
JoseMiguel Marte <tumusicalatina@gmail.com>
2019-09-17 13:59:57 -0400

    first commit

---
+++ c#_edx_microsoft/oop_cSharp/oop_cSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace oop_cSharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var myDrinkMachine = new DrinksMachine("Kitchen","Greca","greca2000");
+            
+            Console.WriteLine(myDrinkMachine.Location);
+            Console.WriteLine(myDrinkMachine.Make);
+            Console.WriteLine(myDrinkMachine.Model);
+
+            
+            myDrinkMachine.MakeCapuccino();
+        }
+    }
+}
